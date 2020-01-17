964298bd454f6350bb3d5c186b8a98d6703f0cb0
Ryan Coughlin <ryan@ryancoughlin.com>
2020-01-17 16:20:31 -0500

    Revert "ddd"
    This reverts commit 2d373c6936ffe0fdd5a6eafd1473abdb707b2211.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,7 @@
-import React from "react";
-import Helmet from "react-helmet";
-import "./index.css";
+import React from 'react'
+import Helmet from 'react-helmet'
+import './index.css'
 
-const Layout = ({ children }) => (
-  <div>
-    <p>fixing this...</p>
-  </div>
-);
+const Layout = ({ children }) => <div>{children()}</div>
 
-export default Layout;
+export default Layout
